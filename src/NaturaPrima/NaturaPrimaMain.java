a7aa95964e0ad441460646b27d8f5238b32b827f
Carl Palad <carl.palad.713@gmail.com>
2025-01-14 13:50:35 -0500

    Made the username input and validation, implemented room/choice system, and removed all setters in the treatment class.

--- src/NaturaPrima/NaturaPrimaMain.java
+++ src/NaturaPrima/NaturaPrimaMain.java
@@ -1,6 +1,8 @@
 package NaturaPrima;
 
 import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * author: Carl Palad
@@ -12,12 +14,15 @@ import java.util.Scanner;
 // date: 10/01/2025; Asked AI which of the current tasks I should focus on. Told me to focus on the main gameplay loop(choice system, turn system, etc.) before 
 // working on the side features(difficulty, emoji pictures, text colour, etc.)
 
+// AI Usage #2
+// date: 14/01/2025; Asked AI how to implement the room system for choosing choices. AI told me to use a while loop to keep the game running until the player
+// reaches the max turns.
+
 public class NaturaPrimaMain {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         // Main Task List
-        //TODO: Make the room system for choosing choices
-        //TODO: Make the choice system
+        //TODO: Make a point system for the treatments to show treatment compatibility
         //TODO: Research and input pros and cons for each treatment
         //TODO: Make the end screen(shows user's ending)
         //TODO: Improve the start screen(Explains the premise of the game)
@@ -30,52 +35,88 @@ public class NaturaPrimaMain {
         //TODO: See if we have time to implement difficulty.
     
         // List of all treatments
-        Treatment treatments[] = {
-            new Treatment("multipond",750, "good", "bad"),
-            new Treatment("lake",2000, "good", "bad"),
-            new Treatment("tree planting",1250, "good", "bad"),
-            new Treatment("tree burning", 500, "good", "bad"),
-            new Treatment("habitat building",3000, "good", "bad"),
-            new Treatment("Introduces Pollinators",750, "good", "bad")
-        };
+        ArrayList<Treatment> treatments = new ArrayList<Treatment>();
+        treatments.add(new Treatment("multipond", 750, "good", "bad"));
+        treatments.add(new Treatment("lake", 2000, "good", "bad"));
+        treatments.add(new Treatment("tree planting", 1250, "good", "bad"));
+        treatments.add(new Treatment("tree burning", 500, "good", "bad"));
+        treatments.add(new Treatment("habitat building", 3000, "good", "bad"));
+        treatments.add(new Treatment("Introduces Pollinators", 750, "good", "bad"));
 
-        // Room/Turn system
-        // Picks a random treatment for option 1
-        int optionOneRandIndex = (int)(Math.random() * treatments.length);
-        System.out.println("Option 1: " + treatments[optionOneRandIndex].getTreatmentName());
+        // List of selected treatments
+        ArrayList<Treatment> selectedTreatments = new ArrayList<Treatment>();
 
-        // Picks a random treatment for option 2
-        int optionTwoRandIndex = (int)(Math.random() * treatments.length);
+        // Game Variables
+        int turnCount = 0;
+        int maxTurns = 10;
 
-        // Ensures that option 1 and 2 is not the same
-        while (optionTwoRandIndex == optionOneRandIndex) {
-            optionTwoRandIndex = (int)(Math.random() * treatments.length); 
+        // Room/Choice System
+        while (turnCount < maxTurns) {
+            // Picks a random treatment for option 1 and 2
+            int optionOneRandIndex = (int)(Math.random() * treatments.size());
+            int optionTwoRandIndex = (int)(Math.random() * treatments.size());
+            
+            // Ensures that option 1 and 2 is not the same
+            while (optionTwoRandIndex == optionOneRandIndex) {
+                optionTwoRandIndex = (int)(Math.random() * treatments.size()); 
+            }
+            
+            System.out.println("Option 1: " + treatments.get(optionOneRandIndex).getTreatmentName());
+            System.out.println("Option 2: " + treatments.get(optionTwoRandIndex).getTreatmentName());
+
+            System.out.print("Choose an option: 1 or 2");
+            int choice = input.nextInt();
+            input.nextLine();
+
+            if (choice == 1) {
+                selectedTreatments.add(treatments.get(optionOneRandIndex));
+            } else if (choice == 2) {
+                selectedTreatments.add(treatments.get(optionTwoRandIndex));
+            } else {
+                System.out.println("Invalid choice. Please choose 1 or 2");
+            }
+
+            turnCount++;
         }
-
-        System.out.println("Option 2: " + treatments[optionTwoRandIndex].getTreatmentName());
-
-        System.out.println("testing testing 1 two 333");
     
         //if choice gets picked, add to treatment to players selectedTreatments
 
-        // Game
-        System.out.println("------------------------------------------------------------------------------------------------------------------------------------\n");
+        // Game Starts
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------------");
         System.out.println("\nWelcome to Natura Prima!\nA text adventure game in which you try to save a dying ecosystem.");
-        System.out.print("Input a username: ");
-        String username = input.nextLine();
-        // TODO: Make a while loop for this
-        System.out.println("You have chosen, " + username + ", are you sure you want this?");
-        System.out.println("yes or no?");
-        String optionOne = input.nextLine();
-        if (optionOne == "yes") {
-            // Continue the game
-        } else {
-            // Rename
+        System.out.println("You have 25,000$ to spend on treatments to save it. Good luck!\n");
+        
+        // Username Input
+        String username = "";
+        boolean validUsername = false;
+        while (validUsername == false) {
+            System.out.print("Input a username: ");
+            username = input.nextLine();
+            // Should I remove this? On the fence about this, feels like the opposite of what I wanted
+            String[] inappropriateWords = {"fuck", "ass", "bitch", "butt", "booty", "nigg", "penis", "pussy", "testicles", "boob", "shit", "pee", "piss", "tits", "semen", "sperm", "arse", "bastard", "cunt", "cock", "cuck", "sucker", "poop", "fag", "weed"};
+            boolean inappropriate = false;
+            for (String word : inappropriateWords) {
+                if (username.toLowerCase().contains(word.toLowerCase())) {
+                    System.out.println("INAPPROPRIATE!!! Choose a different username.");
+                    inappropriate = true;
+                    break;
+                }
+            }
+            if (inappropriate == true) {
+                continue;
+            }
+            
+            System.out.println("You have chosen, " + username + ", are you sure you want this?");
+            System.out.print("Type 'yes' to confirm or 'no' to re-enter: ");
+            String confirmation = input.nextLine();
+            
+            if (confirmation.equalsIgnoreCase("yes")) {
+                validUsername = true;
+            }
         }
 
         input.close();
 
-        // Game Starts
         System.out.println(username + ", You went back to your family home and saw your childhood playplace was devastated. You have 25000 to fix it");
 
     }
--- src/NaturaPrima/Treatment.java
+++ src/NaturaPrima/Treatment.java
@@ -3,12 +3,14 @@ package NaturaPrima;
 public class Treatment {
     private String treatmentName;
     private double cost;
+    private String info;
     private String pros;
     private String cons;
 
-    public Treatment(String treatmentName, double cost, String pros, String cons) {
+    public Treatment(String treatmentName, double cost, String info, String pros, String cons) {
         this.treatmentName = treatmentName;
         this.cost = cost;
+        this.info = info;
         this.pros = pros;
         this.cons = cons;
     }
@@ -17,32 +19,20 @@ public class Treatment {
         return cost;
     }
 
-    public void setCost(double cost) {
-        this.cost = cost;
+    public String getInfo() {
+        return info;
     }
 
     public String getPros() {
         return pros;
     }
 
-    public void setPros(String pros) {
-        this.pros = pros;
-    }
-
     public String getCons() {
         return cons;
-    }
-    
-    public void setCons(String cons) {
-        this.cons = cons;
     }
 
     public String getTreatmentName() {
         return treatmentName;
-    }
-
-    public void setTreatmentName(String treatmentName) {
-        this.treatmentName = treatmentName;
     }
 
     public String toString() {
